1ae70700b3ed091a0aecad8cb16845e4003a3ece
Gauravbisht11 <bgaurav.8911@gmail.com>
2023-07-14 21:07:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route
   
